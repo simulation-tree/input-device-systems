996084fa330aaed2704d892d107f42da75ffd46b
Phill <philldamaskin@gmail.com>
2024-08-24 13:12:56 -0400

    implement global hooks for kbm

---
+++ source/GlobalKeyboardAndMouseSystem.cs
@@ -0,0 +1,316 @@
+﻿using InputDevices.Components;
+using InputDevices.Events;
+using SharpHook;
+using SharpHook.Native;
+using Simulation;
+using System;
+using System.Numerics;
+
+namespace InputDevices.Systems
+{
+    public class GlobalKeyboardAndMouseSystem : SystemBase
+    {
+        private readonly TaskPoolGlobalHook kbmHook;
+        private KeyboardState keyboardControls = default;
+        private MouseState mouseControls = default;
+        private KeyboardState lastKeyboard = default;
+        private MouseState lastMouse = default;
+        private eint keyboardEntity;
+        private eint mouseEntity;
+
+        public GlobalKeyboardAndMouseSystem(World world) : base(world)
+        {
+            keyboardEntity = (Entity)GetOrCreateKeyboard();
+            mouseEntity = (Entity)GetOrCreateMouse();
+            kbmHook = new();
+
+            kbmHook.KeyPressed += OnKeyPressed;
+            kbmHook.KeyReleased += OnKeyReleased;
+            kbmHook.MousePressed += OnMousePressed;
+            kbmHook.MouseReleased += OnMouseReleased;
+            kbmHook.MouseMoved += OnMouseMoved;
+            kbmHook.MouseWheel += OnMouseWheel;
+
+            kbmHook.RunAsync();
+            Subscribe<InputUpdate>(Update);
+        }
+
+        public override void Dispose()
+        {
+            kbmHook.Dispose();
+            base.Dispose();
+        }
+
+        private void Update(InputUpdate update)
+        {
+            UpdateStates();
+        }
+
+        private void UpdateStates()
+        {
+            Keyboard keyboard = GetOrCreateKeyboard();
+            bool keyboardUpdated = false;
+            for (uint i = 0; i < KeyboardState.MaxKeyCount; i++)
+            {
+                bool next = keyboardControls[i];
+                bool previous = lastKeyboard[i];
+                ButtonState state = keyboard.GetButtonState(i);
+                ButtonState current = new(previous, next);
+                if (state != current)
+                {
+                    lastKeyboard[i] = next;
+                    keyboard.SetButtonState(i, current);
+                    keyboardUpdated = true;
+                }
+            }
+
+            if (keyboardUpdated)
+            {
+                InputDevice device = keyboard;
+                DateTimeOffset when = DateTimeOffset.Now;
+                TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                device.SetUpdateTime(timestamp);
+            }
+
+            Mouse mouse = GetOrCreateMouse();
+            bool mouseUpdated = false;
+            for (uint i = 0; i < MouseState.MaxButtonCount; i++)
+            {
+                bool next = mouseControls[i];
+                bool previous = lastMouse[i];
+                ButtonState state = mouse.GetButtonState(i);
+                ButtonState current = new(previous, next);
+                if (state != current)
+                {
+                    lastMouse[i] = next;
+                    mouse.SetButtonState(i, current);
+                    mouseUpdated = true;
+                }
+            }
+
+            if (mouseUpdated)
+            {
+                InputDevice device = mouse;
+                DateTimeOffset when = DateTimeOffset.Now;
+                TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                device.SetUpdateTime(timestamp);
+            }
+        }
+
+        private Keyboard GetOrCreateKeyboard()
+        {
+            if (keyboardEntity == default)
+            {
+                Keyboard keyboard = new(world);
+                keyboardEntity = (Entity)keyboard;
+            }
+
+            return new(world, keyboardEntity);
+        }
+
+        private Mouse GetOrCreateMouse()
+        {
+            if (mouseEntity == default)
+            {
+                Mouse mouse = new(world);
+                mouseEntity = (Entity)mouse;
+            }
+
+            return new(world, mouseEntity);
+        }
+
+        private void OnKeyPressed(object? sender, KeyboardHookEventArgs e)
+        {
+            if (world != default)
+            {
+                Keyboard.Button control = GetControl(e.Data.KeyCode);
+                if (!keyboardControls[(uint)control])
+                {
+                    keyboardControls[(uint)control] = true;
+                    Keyboard keyboard = GetOrCreateKeyboard();
+                    InputDevice device = keyboard;
+                    DateTimeOffset when = e.EventTime;
+                    TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                    device.SetUpdateTime(timestamp);
+                }
+            }
+        }
+
+        private void OnKeyReleased(object? sender, KeyboardHookEventArgs e)
+        {
+            if (world != default)
+            {
+                Keyboard.Button control = GetControl(e.Data.KeyCode);
+                if (keyboardControls[(uint)control])
+                {
+                    keyboardControls[(uint)control] = false;
+                    Keyboard keyboard = GetOrCreateKeyboard();
+                    InputDevice device = keyboard;
+                    DateTimeOffset when = e.EventTime;
+                    TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                    device.SetUpdateTime(timestamp);
+                }
+            }
+        }
+
+        private void OnMousePressed(object? sender, MouseHookEventArgs e)
+        {
+            if (world != default)
+            {
+                uint control = (uint)e.Data.Button;
+                if (!mouseControls[control])
+                {
+                    mouseControls[control] = true;
+                    Mouse mouse = GetOrCreateMouse();
+                    InputDevice device = mouse;
+                    DateTimeOffset when = e.EventTime;
+                    TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                    device.SetUpdateTime(timestamp);
+                }
+            }
+        }
+
+        private void OnMouseReleased(object? sender, MouseHookEventArgs e)
+        {
+            if (world != default)
+            {
+                uint control = (uint)e.Data.Button;
+                if (mouseControls[control])
+                {
+                    mouseControls[control] = false;
+                    Mouse mouse = GetOrCreateMouse();
+                    InputDevice device = mouse;
+                    DateTimeOffset when = e.EventTime;
+                    TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                    device.SetUpdateTime(timestamp);
+                }
+            }
+        }
+
+        private void OnMouseMoved(object? sender, MouseHookEventArgs e)
+        {
+            if (world != default)
+            {
+                Mouse mouse = GetOrCreateMouse();
+                InputDevice device = mouse;
+                mouse.Position = new Vector2(e.Data.X, e.Data.Y);
+                DateTimeOffset when = e.EventTime;
+                TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                device.SetUpdateTime(timestamp);
+            }
+        }
+
+        private void OnMouseWheel(object? sender, MouseWheelHookEventArgs e)
+        {
+            if (world != default)
+            {
+                Mouse mouse = GetOrCreateMouse();
+                InputDevice device = mouse;
+                mouse.Scroll = new Vector2(e.Data.X, e.Data.Y);
+                DateTimeOffset when = e.EventTime;
+                TimeSpan timestamp = when - DateTimeOffset.UnixEpoch;
+                device.SetUpdateTime(timestamp);
+            }
+        }
+
+        private static Keyboard.Button GetControl(KeyCode keyCode)
+        {
+            return keyCode switch
+            {
+                KeyCode.Vc0 => Keyboard.Button.Digit0,
+                KeyCode.Vc1 => Keyboard.Button.Digit1,
+                KeyCode.Vc2 => Keyboard.Button.Digit2,
+                KeyCode.Vc3 => Keyboard.Button.Digit3,
+                KeyCode.Vc4 => Keyboard.Button.Digit4,
+                KeyCode.Vc5 => Keyboard.Button.Digit5,
+                KeyCode.Vc6 => Keyboard.Button.Digit6,
+                KeyCode.Vc7 => Keyboard.Button.Digit7,
+                KeyCode.Vc8 => Keyboard.Button.Digit8,
+                KeyCode.Vc9 => Keyboard.Button.Digit9,
+                KeyCode.VcA => Keyboard.Button.A,
+                KeyCode.VcB => Keyboard.Button.B,
+                KeyCode.VcC => Keyboard.Button.C,
+                KeyCode.VcD => Keyboard.Button.D,
+                KeyCode.VcE => Keyboard.Button.E,
+                KeyCode.VcF => Keyboard.Button.F,
+                KeyCode.VcG => Keyboard.Button.G,
+                KeyCode.VcH => Keyboard.Button.H,
+                KeyCode.VcI => Keyboard.Button.I,
+                KeyCode.VcJ => Keyboard.Button.J,
+                KeyCode.VcK => Keyboard.Button.K,
+                KeyCode.VcL => Keyboard.Button.L,
+                KeyCode.VcM => Keyboard.Button.M,
+                KeyCode.VcN => Keyboard.Button.N,
+                KeyCode.VcO => Keyboard.Button.O,
+                KeyCode.VcP => Keyboard.Button.P,
+                KeyCode.VcQ => Keyboard.Button.Q,
+                KeyCode.VcR => Keyboard.Button.R,
+                KeyCode.VcS => Keyboard.Button.S,
+                KeyCode.VcT => Keyboard.Button.T,
+                KeyCode.VcU => Keyboard.Button.U,
+                KeyCode.VcV => Keyboard.Button.V,
+                KeyCode.VcW => Keyboard.Button.W,
+                KeyCode.VcX => Keyboard.Button.X,
+                KeyCode.VcY => Keyboard.Button.Y,
+                KeyCode.VcZ => Keyboard.Button.Z,
+                KeyCode.VcF1 => Keyboard.Button.F1,
+                KeyCode.VcF2 => Keyboard.Button.F2,
+                KeyCode.VcF3 => Keyboard.Button.F3,
+                KeyCode.VcF4 => Keyboard.Button.F4,
+                KeyCode.VcF5 => Keyboard.Button.F5,
+                KeyCode.VcF6 => Keyboard.Button.F6,
+                KeyCode.VcF7 => Keyboard.Button.F7,
+                KeyCode.VcF8 => Keyboard.Button.F8,
+                KeyCode.VcF9 => Keyboard.Button.F9,
+                KeyCode.VcF10 => Keyboard.Button.F10,
+                KeyCode.VcF11 => Keyboard.Button.F11,
+                KeyCode.VcF12 => Keyboard.Button.F12,
+                KeyCode.VcF13 => Keyboard.Button.F13,
+                KeyCode.VcF14 => Keyboard.Button.F14,
+                KeyCode.VcF15 => Keyboard.Button.F15,
+                KeyCode.VcF16 => Keyboard.Button.F16,
+                KeyCode.VcF17 => Keyboard.Button.F17,
+                KeyCode.VcF18 => Keyboard.Button.F18,
+                KeyCode.VcF19 => Keyboard.Button.F19,
+                KeyCode.VcF20 => Keyboard.Button.F20,
+                KeyCode.VcF21 => Keyboard.Button.F21,
+                KeyCode.VcF22 => Keyboard.Button.F22,
+                KeyCode.VcF23 => Keyboard.Button.F23,
+                KeyCode.VcF24 => Keyboard.Button.F24,
+                KeyCode.VcNumLock => Keyboard.Button.NumLock,
+                KeyCode.VcScrollLock => Keyboard.Button.ScrollLock,
+                KeyCode.VcLeftShift => Keyboard.Button.LeftShift,
+                KeyCode.VcRightShift => Keyboard.Button.RightShift,
+                KeyCode.VcLeftControl => Keyboard.Button.LeftControl,
+                KeyCode.VcRightControl => Keyboard.Button.RightControl,
+                KeyCode.VcLeftAlt => Keyboard.Button.LeftAlt,
+                KeyCode.VcRightAlt => Keyboard.Button.RightAlt,
+                KeyCode.VcLeftMeta => Keyboard.Button.LeftGui,
+                KeyCode.VcRightMeta => Keyboard.Button.RightGui,
+                KeyCode.VcSpace => Keyboard.Button.Space,
+                KeyCode.VcQuote => Keyboard.Button.Apostrophe,
+                KeyCode.VcComma => Keyboard.Button.Comma,
+                KeyCode.VcMinus => Keyboard.Button.Minus,
+                KeyCode.VcPeriod => Keyboard.Button.Period,
+                KeyCode.VcSlash => Keyboard.Button.Slash,
+                KeyCode.VcSemicolon => Keyboard.Button.Semicolon,
+                KeyCode.VcEquals => Keyboard.Button.Equals,
+                KeyCode.VcOpenBracket => Keyboard.Button.LeftBracket,
+                KeyCode.VcBackslash => Keyboard.Button.Backslash,
+                KeyCode.VcCloseBracket => Keyboard.Button.RightBracket,
+                KeyCode.VcBackQuote => Keyboard.Button.Grave,
+                KeyCode.VcEscape => Keyboard.Button.Escape,
+                KeyCode.VcEnter => Keyboard.Button.Enter,
+                KeyCode.VcTab => Keyboard.Button.Tab,
+                KeyCode.VcBackspace => Keyboard.Button.Backspace,
+                KeyCode.VcInsert => Keyboard.Button.Insert,
+                KeyCode.VcDelete => Keyboard.Button.Delete,
+                KeyCode.VcRight => Keyboard.Button.Right,
+                KeyCode.VcLeft => Keyboard.Button.Left,
+                KeyCode.VcDown => Keyboard.Button.Down,
+                KeyCode.VcUp => Keyboard.Button.Up,
+                _ => throw new NotImplementedException($"Key code {keyCode} is not implemented")
+            };
+        }
+    }
+}
---
+++ source/WindowDevicesSystems.cs
@@ -0,0 +1,39 @@
+﻿using InputDevices.Events;
+using SDL3;
+using Simulation;
+using System;
+using static SDL3.SDL3;
+
+namespace InputDevices.Systems
+{
+    public class WindowDevicesSystems : SystemBase
+    {
+        public WindowDevicesSystems(World world) : base(world)
+        {
+            Subscribe<InputUpdate>(Update);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+        }
+
+        private void Update(InputUpdate update)
+        {
+            ReadSDL3DeviceInputs();
+        }
+
+        private unsafe void ReadSDL3DeviceInputs()
+        {
+            byte* state = SDL_GetKeyboardState(null);
+            for (int j = 0; j < (int)SDL_Scancode.NumScancodes; j++)
+            {
+                byte key = state[j];
+                if (key == 1)
+                {
+                    //Console.WriteLine($"Key {(Keyboard.Button)j} is pressed on keyboard");
+                }
+            }
+        }
+    }
+}
